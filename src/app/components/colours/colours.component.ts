037d032a2f95d715781ef3d793708106e27ef9aa
KyleJosling <kylejosling@hotmail.com>
2018-01-29 20:27:54 -0500

    Fixed reference errors, made prettier

--- src/app/components/colours/colours.component.ts
+++ src/app/components/colours/colours.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder,FormGroup,ReactiveFormsModule } from '@angular/forms'
-import {DataService} from '../../services/data.service';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { DataService } from '../../services/data.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-colours',
@@ -11,47 +11,46 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 export class ColoursComponent implements OnInit {
 
-	//array of colours
-	hobbies:number;
-	colors:string[]=[];
-  constructor(private http:HttpClient) {
-		this.hobbies=4;
+  //array of colours
+  colors: string[] = [];
 
-		}
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit() {
   }
 
-	//file for sending post request to server
-	public setFile(event) {
-    let files = event.srcElement.files
-    if (!files) {
-      return
+
+
+  //file for sending post request to server
+  public setFile(event) {
+
+	console.log('loading');
+    interface ReturnedColours {
+      result: string;
     }
 
+    //Return if no files found
+    let files = event.srcElement.files;
+    if (!files) {
+      return;
+    }
+    //Create form data
     const formData: FormData = new FormData();
+    formData.append('file', files[0]);
 
-		formData.append('file',files[0]);
+    //Make post request
+    this.http.post<ReturnedColours>(`https://art-mind.herokuapp.com/stuff`, formData).subscribe(
+      (r) => {
+        console.log('got r', r.result);
 
-    this.http.post(`https://art-mind.herokuapp.com/stuff`,formData).subscribe(
-      (r)=>{
-				console.log('got r', r.result);
-
-				for(var i=0;i<5;i++){
-					console.log(r.result[i]);
-					this.colors[i]='rgb('+r.result[i][0] +','+r.result[i][1]+','+r.result[i][2]+')';
-				}
-				console.log(this.colors);
-
-				this.setColor(this.colors);
-			}
-    )
+        //Fill the colour array
+        for (var i = 0; i < 5; i++) {
+          console.log(r.result[i]);
+          this.colors[i] = 'rgb(' + r.result[i][0] + ',' + r.result[i][1] + ',' + r.result[i][2] + ')';
+        }
+        console.log(this.colors);
+      });
   }
-
-	private setColor(colors){
-
-		console.log('made');
-
-
-	}
 }
